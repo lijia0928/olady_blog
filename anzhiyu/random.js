5920ddbe6085261c88f28413e924ee5b9535d291
olady_blog <3479059983@qq.com>
2024-12-24 22:31:47 +0800

    Site updated: 2024-12-24 22:31:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/16/hello-world/","2024/12/17/我创建的文章/"];function toRandomPost(){
+var posts=["2024/12/17/我创建的文章/","2024/12/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
